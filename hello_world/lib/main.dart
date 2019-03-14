0b25abdebd4ef84bb0a47874f8c97ba98fb7cd60
jiemo <you@example.com>
2019-03-14 15:18:34 +0800

    flutter demo code

---
+++ hello_world/lib/main.dart
@@ -0,0 +1,692 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // final word = new WordPair.random();
+    return new MaterialApp(
+      title: "new world",
+      home: new ScaffoldRoute(),
+      // home: new Scaffold(
+      //   appBar: new AppBar(
+      //     title: new Text("new1 word"),
+      //   ),
+      //   body: new Center(
+      //     child: new ContainerTest()
+      //   ),
+      // ),
+    );
+  }
+}
+
+class ContainerTest extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(top: 50.0, left: 120.0), //容器外补白
+      constraints: BoxConstraints.tightFor(width: 200.0, height: 150.0), //卡片大小
+      decoration: BoxDecoration(
+          //背景装饰
+          gradient: RadialGradient(
+              //背景径向渐变
+              colors: [Colors.red, Colors.orange],
+              center: Alignment.topLeft,
+              radius: .98),
+          boxShadow: [
+            //卡片阴影
+            BoxShadow(
+                color: Colors.black54,
+                offset: Offset(2.0, 2.0),
+                blurRadius: 4.0)
+          ]),
+      transform: Matrix4.rotationZ(.2), //卡片倾斜变换
+      alignment: Alignment.center, //卡片内文字居中
+      child: Text(
+        //卡片文字
+        "5.20", style: TextStyle(color: Colors.white, fontSize: 40.0),
+      ),
+    );
+  }
+}
+
+class SingleChildScrollViewTestRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    String str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    return Scrollbar(
+      child: SingleChildScrollView(
+        padding: EdgeInsets.all(16.0),
+        reverse: false,
+        child: Center(
+          child: Column(
+            //动态创建一个List<Widget>
+            children: str
+                .split("")
+                //每一个字母都用一个Text显示,字体为原来的两倍
+                .map((c) => Text(
+                      c,
+                      textScaleFactor: 2.0,
+                    ))
+                .toList(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ScaffoldRoute extends StatefulWidget {
+  @override
+  _ScaffoldRouteState createState() => _ScaffoldRouteState();
+}
+
+class _ScaffoldRouteState extends State<ScaffoldRoute>
+    with SingleTickerProviderStateMixin {
+  int _selectedIndex = 1;
+  TabController _tabController;
+  List tabs = ["新闻", "历史", "经济"];
+  @override
+  void initState() {
+    super.initState();
+    // 创建Controller
+    _tabController = TabController(length: tabs.length, vsync: this);
+    _tabController.addListener(() {
+      switch (_tabController.index) {
+        case 0:
+          print(_tabController.index); //加入滑动后 点击切换监听会监听两次此方法
+          break;
+        case 1:
+          print(_tabController.index);
+          break;
+        case 2:
+          print(_tabController.index);
+          break;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        //导航栏
+        title: Container(color: Colors.orange, child: Text("App Name")),
+        centerTitle: true,
+        bottom: TabBar(
+            controller: _tabController,
+            tabs: tabs.map((e) => Tab(text: e)).toList()),
+
+        actions: <Widget>[
+          //导航栏右侧菜单
+          IconButton(icon: Icon(Icons.beach_access), onPressed: () {}),
+          IconButton(icon: Icon(Icons.share), onPressed: () {}),
+        ],
+        leading: Builder(builder: (context) {
+          return IconButton(
+            icon: Icon(Icons.dashboard, color: Colors.white), //自定义图标
+            onPressed: () {
+              // 打开抽屉菜单
+              Scaffold.of(context).openDrawer();
+            },
+          );
+        }),
+      ),
+      body: TabBarView(
+        controller: _tabController,
+        children: tabs.map((e) {
+          //创建3个Tab页
+          return Container(
+            alignment: Alignment.center,
+            child: Text(e, textScaleFactor: 5),
+          );
+        }).toList(),
+      ),
+      drawer: new MyDrawer(), //抽屉
+      // bottomNavigationBar: BottomNavigationBar( // 底部导航
+      //   items: <BottomNavigationBarItem>[
+      //     BottomNavigationBarItem(icon: Icon(Icons.home), title: Text('Home')),
+      //     BottomNavigationBarItem(icon: Icon(Icons.business), title: Text('Business')),
+      //     BottomNavigationBarItem(icon: Icon(Icons.school), title: Text('School')),
+      //   ],
+      //   currentIndex: _selectedIndex,
+      //   fixedColor: Colors.blue,
+      //   onTap: _onItemTapped,
+      // ),
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.white,
+        shape: CircularNotchedRectangle(), // 底部导航栏打一个圆形的洞
+        child: Row(
+          children: [
+            IconButton(icon: Icon(Icons.home)),
+            SizedBox(), //中间位置空出
+            IconButton(icon: Icon(Icons.business)),
+          ],
+          mainAxisAlignment: MainAxisAlignment.spaceAround, //均分底部导航栏横向空间
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+          //悬浮按钮
+          child: Icon(Icons.add),
+          onPressed: _onAdd),
+      floatingActionButtonLocation:FloatingActionButtonLocation.centerDocked,//将浮动按钮移动至中间空洞位置
+
+    );
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  void _onAdd() {}
+}
+
+class MyDrawer extends StatelessWidget {
+  const MyDrawer({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: MediaQuery.removePadding(
+        context: context,
+        // DrawerHeader consumes top MediaQuery padding.
+        removeTop: true,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(top: 38.0),
+              child: Row(
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                    child: ClipOval(
+                      child: Icon(Icons.block,size:(80.0))
+                    ),
+                  ),
+                  Text(
+                    "Wendux",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  )
+                ],
+              ),
+            ),
+            Expanded(
+              child: ListView(
+                children: <Widget>[
+                  ListTile(
+                    leading: const Icon(Icons.add),
+                    title: const Text('Add account'),
+                  ),
+                  ListTile(
+                    leading: const Icon(Icons.settings),
+                    title: const Text('Manage accounts'),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class TransformTest extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        DecoratedBox(
+            decoration: BoxDecoration(color: Colors.red),
+            child: Transform.scale(scale: 1.5, child: Text("Hello world"))),
+        Text(
+          "你好",
+          style: TextStyle(color: Colors.green, fontSize: 18.0),
+        )
+      ],
+    );
+  }
+}
+
+class DecoratedBoxTest extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return DecoratedBox(
+        decoration: BoxDecoration(
+            gradient:
+                LinearGradient(colors: [Colors.red, Colors.orange[700]]), //背景渐变
+            borderRadius: BorderRadius.circular(3.0), //3像素圆角
+            boxShadow: [
+              //阴影
+              BoxShadow(
+                  color: Colors.black54,
+                  offset: Offset(2.0, 2.0),
+                  blurRadius: 4.0)
+            ]),
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 80.0, vertical: 18.0),
+          child: Text(
+            "Login",
+            style: TextStyle(color: Colors.white),
+          ),
+        ));
+  }
+}
+
+class FlowTest extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Flow(
+      delegate: TestFlowDelegate(margin: EdgeInsets.all(10.0)),
+      children: <Widget>[
+        new Container(
+          width: 80.0,
+          height: 80.0,
+          color: Colors.red,
+        ),
+        new Container(
+          width: 80.0,
+          height: 80.0,
+          color: Colors.green,
+        ),
+        new Container(
+          width: 80.0,
+          height: 80.0,
+          color: Colors.blue,
+        ),
+        new Container(
+          width: 80.0,
+          height: 80.0,
+          color: Colors.yellow,
+        ),
+        new Container(
+          width: 80.0,
+          height: 80.0,
+          color: Colors.brown,
+        ),
+        new Container(
+          width: 80.0,
+          height: 80.0,
+          color: Colors.purple,
+        ),
+      ],
+    );
+  }
+}
+
+class TestFlowDelegate extends FlowDelegate {
+  EdgeInsets margin = EdgeInsets.zero;
+  TestFlowDelegate({this.margin});
+
+  @override
+  void paintChildren(FlowPaintingContext context) {
+    var x = margin.left;
+    var y = margin.top;
+    //计算每一个子widget的位置
+    for (int i = 0; i < context.childCount; i++) {
+      var w = context.getChildSize(i).width + x + margin.right;
+      if (w < context.size.width) {
+        context.paintChild(i,
+            transform: new Matrix4.translationValues(x, y, 0.0));
+        x = w + margin.left;
+      } else {
+        x = margin.left;
+        y += context.getChildSize(i).height + margin.top + margin.bottom;
+        //绘制子widget(有优化)
+        context.paintChild(i,
+            transform: new Matrix4.translationValues(x, y, 0.0));
+        x += context.getChildSize(i).width + margin.left + margin.right;
+      }
+    }
+  }
+
+  @override
+  bool shouldRepaint(FlowDelegate oldDelegate) {
+    return oldDelegate != this;
+  }
+
+  @override
+  Size getSize(BoxConstraints constraints) {
+    return Size(double.infinity, 200.0);
+  }
+}
+
+class WrapTest extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Wrap(
+      direction: Axis.horizontal,
+      spacing: 10.0, // 主轴(水平)方向间距ßß
+      runSpacing: 10.0, // 纵轴（垂直）方向间距
+      alignment: WrapAlignment.center, //沿主轴方向居中
+      crossAxisAlignment: WrapCrossAlignment.start,
+      verticalDirection: VerticalDirection.down,
+      children: <Widget>[
+        new Chip(
+          avatar:
+              new CircleAvatar(backgroundColor: Colors.blue, child: Text('A')),
+          label: new Text('Hamilton'),
+        ),
+        new Chip(
+          avatar:
+              new CircleAvatar(backgroundColor: Colors.blue, child: Text('M')),
+          label: new Text('Lafayette'),
+        ),
+        new Chip(
+          avatar:
+              new CircleAvatar(backgroundColor: Colors.blue, child: Text('H')),
+          label: new Text('Mulligan'),
+        ),
+        new Chip(
+          avatar:
+              new CircleAvatar(backgroundColor: Colors.blue, child: Text('J')),
+          label: new Text('Laurens'),
+        ),
+      ],
+    );
+  }
+}
+
+class RowOut extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        // Expanded(
+        // flex: 1,
+        Text("xxx" * 100),
+        // )
+      ],
+    );
+  }
+}
+
+class FlexLayoutTestRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        //Flex的两个子widget按1：2来占据水平空间
+        Flex(
+          direction: Axis.horizontal,
+          children: <Widget>[
+            Expanded(
+              flex: 2,
+              child: Container(
+                height: 30.0,
+                color: Colors.red,
+              ),
+            ),
+            Expanded(
+              flex: 2,
+              child: Container(
+                height: 30.0,
+                color: Colors.green,
+              ),
+            ),
+          ],
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 50.0),
+          child: SizedBox(
+            height: 30.0,
+            //Flex的三个子widget，在垂直方向按2：1：1来占用100像素的空间
+            child: Flex(
+              direction: Axis.vertical,
+              children: <Widget>[
+                Expanded(
+                  flex: 2,
+                  child: Container(
+                    height: 00.0,
+                    color: Colors.red,
+                  ),
+                ),
+                Spacer(
+                  flex: 2,
+                ),
+                Expanded(
+                  flex: 1,
+                  child: Container(
+                    height: 00.0,
+                    color: Colors.green,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class InsertColum extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.green,
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.max, //有效，外层Colum高度为整个屏幕
+          children: <Widget>[
+            new Text("data"),
+            Container(
+              color: Colors.red,
+              child: Column(
+                mainAxisSize: MainAxisSize.max, //无效，内层Colum高度为实际高度
+                children: <Widget>[
+                  Text("hello world "),
+                  Text("I am Jack "),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class RowTest extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      //测试Row对齐方式，排除Column默认居中对齐的干扰
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(" hello world "),
+            Text(" I am MainAxisAlignment.center "),
+          ],
+        ),
+        Row(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(" hello world "),
+            Text(" I am Jack "),
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          textDirection: TextDirection.rtl,
+          children: <Widget>[
+            Text(" hello world "),
+            Text(" I am Jack MainAxisAlignment.end"),
+          ],
+        ),
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          verticalDirection: VerticalDirection.down,
+          children: <Widget>[
+            Text(
+              " hello world ",
+              style: TextStyle(fontSize: 30.0),
+            ),
+            Text(" I am Jack "),
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+class ParentWidgetC extends StatefulWidget {
+  @override
+  _ParentWidgetCState createState() => new _ParentWidgetCState();
+}
+
+class _ParentWidgetCState extends State<ParentWidgetC> {
+  bool _active = false;
+
+  void _handleTapboxChanged(bool newValue) {
+    setState(() {
+      _active = newValue;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      child: new TapboxC(
+        active: _active,
+        onChanged: _handleTapboxChanged,
+      ),
+    );
+  }
+}
+
+//----------------------------- TapboxC ------------------------------
+
+class TapboxC extends StatefulWidget {
+  TapboxC({Key key, this.active: false, @required this.onChanged})
+      : super(key: key);
+
+  final bool active;
+
+  final ValueChanged<bool> onChanged;
+  _TapboxCState createState() {
+    print(onChanged);
+    return new _TapboxCState();
+  }
+}
+
+class _TapboxCState extends State<TapboxC> {
+  bool _highlight = false;
+
+  void _handleTapDown(TapDownDetails details) {
+    setState(() {
+      _highlight = true;
+    });
+  }
+
+  void _handleTapUp(TapUpDetails details) {
+    setState(() {
+      _highlight = false;
+    });
+  }
+
+  void _handleTapCancel() {
+    setState(() {
+      _highlight = false;
+    });
+  }
+
+  void _handleTap() {
+    widget.onChanged(!widget.active);
+    print(this.widget);
+  }
+
+  Widget build(BuildContext context) {
+    // 在按下时添加绿色边框，当抬起时，取消高亮
+    return new GestureDetector(
+      onTapDown: _handleTapDown, // 处理按下事件
+      onTapUp: _handleTapUp, // 处理抬起事件
+      onTap: _handleTap,
+      onTapCancel: _handleTapCancel,
+      child: new Container(
+        child: new Center(
+          child: new Text(widget.active ? 'Active' : 'Inactive',
+              style: new TextStyle(fontSize: 32.0, color: Colors.white)),
+        ),
+        width: 200.0,
+        height: 200.0,
+        decoration: new BoxDecoration(
+          color: widget.active ? Colors.lightGreen[700] : Colors.grey[600],
+          border: _highlight
+              ? new Border.all(
+                  color: Colors.black,
+                  width: 10.0,
+                )
+              : null,
+        ),
+      ),
+    );
+  }
+}
+
+class SwitchAndCheckBoxTestRoute extends StatefulWidget {
+  @override
+  _SwitchAndCheckBoxTestRouteState createState() =>
+      new _SwitchAndCheckBoxTestRouteState();
+}
+
+class _SwitchAndCheckBoxTestRouteState
+    extends State<SwitchAndCheckBoxTestRoute> {
+  bool _switchSelected = true; //维护单选开关状态
+  bool _checkboxSelected = true; //维护复选框状态
+  final TextEditingController _unameController = new TextEditingController();
+
+  @override
+  void initState() {
+    _unameController.addListener(() {
+      print("这次是:$_unameController.text");
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        TextField(
+          controller: _unameController,
+          autofocus: true,
+          maxLength: 4,
+          maxLengthEnforced: false,
+          onChanged: (value) {
+            print(value);
+          },
+          onEditingComplete: () {
+            print("value");
+          },
+          onSubmitted: (vlue) {
+            print(vlue);
+          },
+          decoration: InputDecoration(
+              labelText: "用户名",
+              hintText: "用户名或邮箱",
+              prefixIcon: Icon(Icons.person)),
+        ),
+        TextField(
+          decoration: InputDecoration(
+              labelText: "密码",
+              hintText: "您的登录密码",
+              prefixIcon: Icon(Icons.lock)),
+          obscureText: true,
+        ),
+      ],
+    );
+  }
+}
